4b6d9fcd15bcd7d381e30045454311b1312f1e37
Martin Cruz M <codemcm@hotmail.com>
2021-06-02 09:53:14 -0600

    conecction class

---
+++ Crud.Database/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+namespace Crud.Database
+{
+    public class Class1
+    {
+        private readonly SqlConnection Conexion = new SqlConnection("Server=DESKTOP-UEPK13H\\RONETJOHN;DataBase= Practica;Integrated Security=true");
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+        }
+    }
+}
